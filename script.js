df1aa059f4d48fe1a47145b318e7a975eec34273
Leoemanuel21 <leoemanuel14.rocha@gmail.com>
2025-09-12 14:10:40 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function calcular(){
+const numberDOM = document.querySelectorAll('.menu_number');
+const resultDOM = document.querySelector('.result');
+let activeRatingNumber;
+
+function select(number){
+    const selectNumberDOM = numberDOM[number - 1];
+    selectNumberDOM.classList.add('numberActive');
+
+    if(activeRatingNumber){
+        const activeRatingDOM = numberDOM[activeRatingNumber - 1];
+        activeRatingDOM.classList.remove('numberActive');
+    }
+
+    activeRatingNumber = number;
+}
+
+function subimit(){
+    if(!activeRatingNumber) return;
+
     const main = document.querySelector("main");
     const resposta = document.getElementById("resposta");
     main.style.display = "none";
     resposta.style.display = "flex";
+
+    resultDOM.textContent = activeRatingNumber;
 }
